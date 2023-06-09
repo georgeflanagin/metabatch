9ef280e129a0ee1f6342afbe74856af959da1155
alina <alina@coltrane.richmond.edu>
2023-06-09 15:11:07 -0400

    sbatch file

---
+++ sbatch.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import typing
+from   typing import *
+
+min_py = (3, 8)
+
+###
+# Standard imports, starting with os and sys
+###
+import os
+import sys
+if sys.version_info < min_py:
+    print(f"This program requires Python {min_py[0]}.{min_py[1]}, or higher.")
+    sys.exit(os.EX_SOFTWARE)
+
+###
+# Other standard distro imports
+###
+import argparse
+import contextlib
+import getpass
+mynetid = getpass.getuser()
+
+###
+# From hpclib
+###
+import linuxutils
+from   urdecorators import trap
+import fifo
+###
+# imports and objects that are a part of this project
+###
+verbose = False
+
+###
+# Credits
+###
+__author__ = 'Alina Enikeeva' 
+__copyright__ = 'Copyright 2023'
+__credits__ = None
+__version__ = 0.1
+__maintainer__ = 'Alina Enikeeva' 
+__email__ = ['alina@richmond.edu']
+__status__ = 'in progress'
+__license__ = 'MIT'
+
+
+@trap
+def sbatch_main(myargs:argparse.Namespace) -> int:
+    p = fifo.FIFO(os.path.join(os.environ.get('METABATCHPATH'), 'metapipe'), 
+        mode='w',
+        verbose=True)
+    data = p(f"{mynetid},{myargs.input}") 
+    print(data)
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(prog="sbatch", 
+        description="What sbatch does, sbatch does best.")
+
+    parser.add_argument('input', type=str, 
+        help="Input file name.")
+    parser.add_argument('-o', '--output', type=str, default="",
+        help="Output file name")
+    parser.add_argument('-v', '--verbose', action='store_true',
+        help="Be chatty about what is taking place")
+
+
+    myargs = parser.parse_args()
+    verbose = myargs.verbose
+
+    try:
+        outfile = sys.stdout if not myargs.output else open(myargs.output, 'w')
+        with contextlib.redirect_stdout(outfile):
+            sys.exit(globals()[f"{os.path.basename(__file__)[:-3]}_main"](myargs))
+
+    except Exception as e:
+        print(f"Escaped or re-raised exception: {e}")
+
